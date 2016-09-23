155a043444e56e9b0bb5ea92eea1de66dd01372e
Alo Haz <alongkar.hajoary@gmail.com>
2016-09-23 22:58:27 +0530

    Update contact_me.php

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -19,6 +19,7 @@ $message = strip_tags(htmlspecialchars($_POST['message']));
 $to = 'alongkar.hajoary@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+$headers .= "From: $email_address"; 
 $headers .= "Reply-To: $email_address";   
 mail($to,$email_subject,$email_body,$headers);
 return true;         
